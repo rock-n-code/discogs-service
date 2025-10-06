80ab289ae80b4aa3deb263ad143be397310c2323
Javier Cicchelli <javier@rock-n-code.com>
2025-10-06 18:10:59 +0200

    Added the files headers to the existing files in the project.

--- Package.swift
+++ Package.swift
@@ -1,5 +1,19 @@
 // swift-tools-version: 5.10
 
+// ===----------------------------------------------------------------------===
+// 
+// This source file is part of the DiscogsService open source project
+// 
+// Copyright (c) 2025 Röck+Cöde VoF. and the DiscogsService project authors
+// Licensed under Apache license v2.0
+// 
+// See LICENSE for license information
+// See CONTRIBUTORS for the list of DiscogsService project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+// 
+// ===----------------------------------------------------------------------===
+
 import PackageDescription
 
 let package = Package(
